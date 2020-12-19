8a8ce77ef1a4b959c8531e975b502e83b5a8d8cf
pedroribeiro89 <pedroribeiro89@hotmail.com>
2020-12-19 11:16:42 -0300

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
-// export const environment = {
-//   production: true,
-//   allowedDomains: ['equale-back-dev.herokuapp.com'],
-//   baseUrl: 'https://equale-back-dev.herokuapp.com'
-// };
 export const environment = {
   production: true,
-  allowedDomains: ['localhost:3000'],
-  baseUrl: 'http://localhost:3000'
+  allowedDomains: ['equale-back-dev.herokuapp.com'],
+  baseUrl: 'https://equale-back-dev.herokuapp.com'
 };
+// export const environment = {
+//   production: true,
+//   allowedDomains: ['localhost:3000'],
+//   baseUrl: 'http://localhost:3000'
+// };
